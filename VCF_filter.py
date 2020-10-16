8eca564c9d58c4c43a042bdd341fe68e85ae35d7
jingyouu <47611714+jingyouu@users.noreply.github.com>
2020-10-16 01:04:10 -0400

    Add command line args

--- VCF_filter.py
+++ VCF_filter.py
@@ -1,15 +1,24 @@
 import os
 import sys
+import argparse
 
-plink = "../plink/plink2.0/"
-vcf_file = "../eMERGE/merged_vcf_gz/chr22.dose.emerge_ids.consented.merged.vcf.gz"
-maf = 0.1
-hwe = 0.000001
-rs = 0.8
+parser = argparse.ArgumentParser(description='Filter VCF files')
+parser.add_argument('--maf', type=str, help='Maf threshold')
+parser.add_argument('--hwe', type=int, help='hwe threshold')
+parser.add_argument('--rs', type=str, help='rs threshold')
+parser.add_argument('--p', type=str, help='Plink path')
+parser.add_argument('--i', type=str, help='Input namefor files to be filter')
+parser.add_argument('--o', type=str, help='Output dir for files filtered')
+args = parser.parse_args()
 
-command = plink + "plink2 --vcf " + vcf_file + " --maf " + str(maf) + " --hwe " + str(hwe) + " --minimac3-r2-filter " + str(rs) + " --make-bed --out VCF/chr22.dose.emerge_ids.consented.merged"
+plink = args.p
+maf = args.maf
+hwe = args.hwe
+rs = args.rs
 
-os.system(command)
-
+for i in range(1,22):
+    vcf_file = "../eMERGE/merged_vcf_gz/chr"+ str(i) +".dose.emerge_ids.consented.merged.vcf.gz"
+    command = plink + "plink2 --vcf " + vcf_file + " --maf " + str(maf) + " --hwe " + str(hwe) + " --minimac3-r2-filter " + str(rs) + " --make-bed --out " args.o + str(i) +".dose.emerge_ids.consented.merged"
+    os.system(command)
 
 
